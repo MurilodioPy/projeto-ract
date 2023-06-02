b1b7b491ae248ce33ad82b9536310a8043b2cb3a
Murilo Dionísio <86753677+MurilodioPy@users.noreply.github.com>
2023-06-02 09:40:45 -0300

    update refatorando pag e ajustando estilo

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import logo from './img/ui-ux-logo.svg';
 const Header = () => {
   return (
     <header>
-      <div className="logoname">
+      <div id="logoname">
         <img src={logo} alt="logo uiux"/>
         <h1> &lt;PJ-REACT/&gt; </h1>
       </div>
@@ -19,25 +19,19 @@ const Header = () => {
         <ul>
           <li>
             <a href="/about" className="nav-link">
-              Home
-            </a>
+              Home </a>
           </li>
           <li>
-            <a
-              href="https://www.treinaweb.com.br/blog/ux-e-ui-o-que-e-e-quais-as-diferencas"
+            <a href="https://www.treinaweb.com.br/blog/ux-e-ui-o-que-e-e-quais-as-diferencas"
               className="nav-link"
-              target="_blank"
-            >
-              Leia Sobre
-            </a>
+              target="_blank" rel="noreferrer">
+              Leia Sobre </a>
           </li>
           <li>
             <a href="https://api.whatsapp.com/send?phone=5534988148930&text=%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB%F0%9F%92%AC%20Conecte-se%20com%20o%20Programador!%20%F0%9F%92%AC%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB%0A%0A%E2%9C%89%EF%B8%8F%20Voc%C3%AA%20est%C3%A1%20convidado(a)%20a%20entrar%20em%20contato%20com%20um%20talentoso%20programador%20que%20pode%20transformar%20suas%20ideias%20em%20realidade!%20%F0%9F%92%A1%0A%0A%F0%9F%8C%90%20Se%20voc%C3%AA%20precisa%20de%20solu%C3%A7%C3%B5es%20tecnol%C3%B3gicas%20personalizadas,%20desenvolvimento%20de%20software%20ou%20qualquer%20projeto%20relacionado%20%C3%A0%20programa%C3%A7%C3%A3o,%20n%C3%A3o%20perca%20essa%20oportunidade.%0A%0A%F0%9F%92%BB%F0%9F%92%A1%20Entre%20em%20contato%20agora%20mesmo%20e%20descubra%20como%20podemos%20colaborar%20para%20alcan%C3%A7ar%20seus%20objetivos!%0A%0A%F0%9F%93%A7%20%5Bmurilo.dionisio@estudante.iftm.edu.br%5D%0A%0A%E2%8C%A8%EF%B8%8F%F0%9F%92%BB%20N%C3%A3o%20deixe%20suas%20ideias%20esperando,%20d%C3%AA%20vida%20a%20elas%20com%20a%20expertise%20de%20um%20programador%20experiente.%20Estamos%20ansiosos%20para%20ouvir%20sobre%20seu%20projeto!%0A%0A%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB%F0%9F%8C%9F%20Juntos,%20podemos%20criar%20algo%20incr%C3%ADvel!%20%F0%9F%8C%9F%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB" 
               className="nav-link"
-              target="_blank"
-            >
-              Contato
-            </a>
+              target="_blank" rel="noreferrer">
+              Contato </a>
           </li>
         </ul>
       </nav>
@@ -102,9 +96,8 @@ const Logos = () => {
     <div className="link-logo">
       {logoData.map((data, index) => (
         <div key={index}>
-          <a href={data.linkLogo} target="_blank">
+          <a href={data.linkLogo} target="_blank" rel="noreferrer">
             <img src={data.logoUrl} alt={data.logoNome}/>
-            <p>{data.logoNome}</p>
           </a>
             {/* adicionando texto por arquivo */}
         </div>

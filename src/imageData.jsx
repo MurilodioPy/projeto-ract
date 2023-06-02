36d3d3fef636ef4418879f1f5b724de353b0b907
Murilo Dionísio <86753677+MurilodioPy@users.noreply.github.com>
2023-06-01 21:12:03 -0300

    Update imageData.jsx

--- src/imageData.jsx
+++ src/imageData.jsx
@@ -1,21 +1,21 @@
 export const imageData = [
     {
-      imageUrl: '../src/img/image6.png',
+      imageUrl: './img/image6.png',
       imageDesc: 'Imagem com texto ux ux e alguns ícones',
       additionalText: 'Diferenças: UX design (user experience) usa ferramentas como pesquisa e personas para identificar pontos importantes e dores ao longo da experiência com o produto. Já UI design (user interface) lida com o visual de um projeto e entrega a parte gráfica, o profissional de UI pensa em como dispor os elementos, tipos de informação, cores e etc.  '
     },
     {
-        imageUrl: '../src/img/image5.jpg',
+        imageUrl: './img/image5.jpg',
         imageDesc: 'Imagem com bolos inteiros e cortados representando ui e ux',
         additionalText: 'Para entendermos melhor como eles estão interligados vamos fazer uma analogia com esses três bolos. Suas interfaces de usuário são diferentes (cobertura, formato e decoração), ou seja, a apresentação, visual e até as dicas de como se interagir com eles, como, por exemplo, em um deles há cinco cerejas distribuídas uniformemente em seu topo, indicando que aquele é um bolo de 5 fatias. Já o UX é como se cortássemos o bolo e olhássemos a fatia e o que há dentro dele, o centro da experiência. Só assim podemos saber se ele está completamente assado, qual é o recheio, se os ingredientes são de qualidade e se o sabor é agradável.'
     },
     {
-        imageUrl: '../src/img/image3.jpg',
+        imageUrl: './img/image3.jpg',
         imageDesc: 'Imagem com cores e textos explicando a teoria das cores',
         additionalText: 'Teoria da Psicologia da Cores: A teoria da psicologia das cores explora como as cores afetam as emoções, comportamentos e percepções dos usuários. As cores são usadas estrategicamente para transmitir mensagens, criar ênfase, estabelecer hierarquia visual e influenciar a experiência geral do usuário. Observe a tabela acima:'
     },
     {
-      imageUrl: '../src/img/image2.jpg',
+      imageUrl: './img/image2.jpg',
       imageDesc: 'Imagem com imagens de telas de aplicativos geradas por IA',
       additionalText: `Design da interface do usuário para o aplicativo de pedidos de comida.  
       Comando utilizado para gerar as imagens: /imagine Food delivery app, mobile app, user interface, Figma, HQ, 4K, clean UI -- q 2`
@@ -24,17 +24,17 @@ export const imageData = [
 
 export const logoData = [
     {
-      logoUrl: '../src/img/react-logo.png',
+      logoUrl: './img/react-logo.png',
       logoNome: 'Doc. React',
       linkLogo: 'https://legacy.reactjs.org/docs/getting-started.html'
     },
     {
-      logoUrl: '../src/img/vite-logo.png',
+      logoUrl: './img/vite-logo.png',
       logoNome: 'Doc. Vite',
       linkLogo: 'https://vitejs.dev/guide/'
     },
     {
-      logoUrl: '../src/img/github-logo.png',
+      logoUrl: './img/github-logo.png',
       logoNome: 'Repositório do projeto',
       linkLogo: 'https://github.com/MurilodioPy/projeto-react'
     }
